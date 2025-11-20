ed80d8d19424b988e8f6340dac18d676ccf8d44f
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-11-20 13:03:59 -0300

    Manutenção do acesso às listas

--- V2/usuarios.py
+++ V2/usuarios.py
@@ -9,14 +9,19 @@ def userRegister(dados_usuario, nomeusuario, emailusuario, senhausuario, tipousu
 def userLogin(dados_usuario, emailusuario, senhausuario):
     for i in dados_usuario['E-mail']:
         if i == emailusuario:
+            print('E-MAIL ENCONTRADO COM SUCESSO!')
             for j in dados_usuario['Senha']:
                 if j == senhausuario:
                     print('USUÁRIO ENCONTRADO COM SUCESSO!')
-                    nome_index = dados_usuario['Nome de usuário'].index(emailusuario)
-                    print(f'NOME DE USUÁRIO: {nome_index}\nE-MAIL: {dados_usuario['E-mail'].get(nome_index)}\n')
-                    if dados_usuario['Tipo de usuário'].get(nome_index) == 1:
+                    nome_index = dados_usuario['E-mail'].index(i)
+                    print(f'NOME DE USUÁRIO: {dados_usuario['Nome de usuário'][nome_index]}\nE-MAIL: {emailusuario}\n')
+                    if dados_usuario['Tipo de usuário'][nome_index] == 1:
                         print('TIPO DE USUÁRIO: ADM')
                         return 1
-                    if dados_usuario['Tipo de usuário'].get(nome_index) == 2:
+                        break
+                    if dados_usuario['Tipo de usuário'][nome_index] == 2:
                         print('TIPO DE USUÁRIO: ADM')
                         return 2
+                        break
+        else:
+            print('E-MAIL NÃO ENCONTRADO')
