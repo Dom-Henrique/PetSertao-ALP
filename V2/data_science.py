8a4fca76391298546803f5f4a64cf4bbda6894d9
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-12-09 07:49:44 -0300

    Manutenção de funcionalidades
    Corrigido o erro de invocação de funções

--- V2/data_science.py
+++ V2/data_science.py
@@ -1,31 +1,34 @@
 import pandas as pd
+from csv2pdf import convert
 
 def Usuarios_Info(dados_usuario_df):
-    print(pd.dados_usuario_df.describe())
+    print(dados_usuario_df.describe())
     
     while True:
         print('DESEJA FILTRAR POR ALGUMA CONDIÇÃO?')
-        option = int(input('1 - NOMES DE USUÁRIO\n2 - E-MAILS\n3 - NOME ESPECÍFICO\nE-MAIL ESPECÍFICO\n'))
+        option = int(input('1 - NOMES DE USUÁRIO\n2 - E-MAILS\n3 - NOME ESPECÍFICO\n4 - E-MAIL ESPECÍFICO\n\n'))
         
         if option == 1:
-            print(dados_usuario_df['Nome de Usuário'])
-            print(dados_usuario_df['Nome de Usuário'].describe())
+            print(dados_usuario_df['Nome de usuário'])
+            print(dados_usuario_df['Nome de usuário'].describe())
         elif option == 2:
             print(dados_usuario_df['E-mail'])
             print(dados_usuario_df['E-mail'].describe())
         elif option == 3:
             nome_desejado = input('Insira o nome desejado: ')
             if nome_desejado in dados_usuario_df:
-                print(dados_usuario_df.loc['Nome de Usuário'] == nome_desejado)
-                print(dados_usuario_df.loc['Nome de Usuário'].describe() == nome_desejado)
+                print(dados_usuario_df.loc['Nome de usuário'] == nome_desejado)
+                print(dados_usuario_df.loc['Nome de usuário'].describe() == nome_desejado)
         elif option == 4:
             email_desejado = input('Insira o nome desejado: ')
             if email_desejado in dados_usuario_df:
                 print(dados_usuario_df.loc['E-mail'] == email_desejado)
                 print(dados_usuario_df.loc['E-mail'].describe() == email_desejado)
+        else:
+            print('OPÇÃO INVÁLIDA')
+            break
                 
 def Abrir_Relatorio():
-    with open('Tabelas/DadosUsuario.csv', 'r') as file:
+    with open('DadosUsuario.csv', 'r') as file:
         leitura_arquivo = file.read()
         print(leitura_arquivo)
-        
