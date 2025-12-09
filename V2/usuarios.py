ba21d0408a02c767a44493ea87e7921619d63699
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-12-09 14:01:55 -0300

    Adição de um ID
    ID gerado com Randint no arquivo principal e posteriormente utilizado para registro do usuário

--- V2/usuarios.py
+++ V2/usuarios.py
@@ -1,10 +1,14 @@
 import cv2
+from recog_fala import *
+from random import randint
+from time import sleep
 
-def userRegister(dados_usuario, nomeusuario, emailusuario, senhausuario, tipousuario):
+def userRegister(dados_usuario, nomeusuario, emailusuario, senhausuario, tipousuario, id_usuario):
     dados_usuario['Nome de usuário'].append(nomeusuario)
     dados_usuario['E-mail'].append(emailusuario)
     dados_usuario['Senha'].append(senhausuario)
     dados_usuario['Tipo de usuário'].append(tipousuario)
+    dados_usuario['ID'].append(id_usuario)
     
     return dados_usuario
 
@@ -14,8 +18,10 @@ def userLogin(dados_usuario, emailusuario, senhausuario):
             print('E-MAIL ENCONTRADO COM SUCESSO!')
             for j in dados_usuario['Senha']:
                 if j == senhausuario:
-                    print('USUÁRIO ENCONTRADO COM SUCESSO!')
+                    print('E-MAIL ENCONTRADO COM SUCESSO!')
                     nome_index = dados_usuario['E-mail'].index(i)
+                    print('REPITA O SEU ID')
+                    reconhecimento_vocal(dados_usuario, nome_index)
                     print(f'NOME DE USUÁRIO: {dados_usuario['Nome de usuário'][nome_index]}\nE-MAIL: {emailusuario}\n')
                     if dados_usuario['Tipo de usuário'][nome_index] == 1:
                         print('TIPO DE USUÁRIO: ADM')
