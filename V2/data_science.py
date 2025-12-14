cfaf908c1312ffab14bf2a51f74c096e63833d7c
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-12-14 15:46:34 -0300

    Usuarios_Info
    Movido a função Usuarios_Info para dentro de Gerar Relatorio

--- V2/data_science.py
+++ V2/data_science.py
@@ -1,31 +1,5 @@
 import pandas as pd
 import os
-
-def Usuarios_Info(dados_usuario_df):
-    print(dados_usuario_df.describe())
-    
-    while True:
-        print('DESEJA FILTRAR POR ALGUMA CONDIÇÃO?')
-        option = int(input('1 - NOMES DE USUÁRIO\n2 - E-MAILS\n3 - NOME ESPECÍFICO\n4 - E-MAIL ESPECÍFICO\n5 - SAIR\n'))
-        
-        if option == 1:
-            print(dados_usuario_df['Nome de usuário'])
-            print(dados_usuario_df['Nome de usuário'].describe())
-        elif option == 2:
-            print(dados_usuario_df['E-mail'])
-            print(dados_usuario_df['E-mail'].describe())
-        elif option == 3:
-            nome_desejado = input('Insira o nome desejado: ')
-            if nome_desejado in dados_usuario_df:
-                print(dados_usuario_df.loc['Nome de usuário'] == nome_desejado)
-                print(dados_usuario_df.loc['Nome de usuário'].describe() == nome_desejado)
-        elif option == 4:
-            email_desejado = input('Insira o nome desejado: ')
-            if email_desejado in dados_usuario_df:
-                print(dados_usuario_df.loc['E-mail'] == email_desejado)
-                print(dados_usuario_df.loc['E-mail'].describe() == email_desejado)
-        elif option == 5:
-            break
                 
 def GerarRelatorio(produto, servico, pets_venda, dados_usuario, profissionais):
     estrutura_dados = input('Relatório desejado: ').lower()
@@ -46,3 +20,35 @@ def GerarRelatorio(produto, servico, pets_venda, dados_usuario, profissionais):
         profissionaisDF.to_csv('Tabelas/profissionais.csv', index=False)
     else:
         print('INDISPONÍVEL')
+        
+    imp_rel = input('IMPRIMIR RELATÓRIO?\nS\tN').lower()
+    if imp_rel == 's':
+        Usuarios_Info(usuariosDF)
+    elif imp_rel == 'n':
+        pass
+        
+    def Usuarios_Info(usuarioDF):
+        print(usuarioDF.describe())
+        
+        while True:
+            print('DESEJA FILTRAR POR ALGUMA CONDIÇÃO?')
+            option = int(input('1 - NOMES DE USUÁRIO\n2 - E-MAILS\n3 - NOME ESPECÍFICO\n4 - E-MAIL ESPECÍFICO\n5 - SAIR\n'))
+            
+            if option == 1:
+                print(usuarioDF['Nome de usuário'])
+                print(usuarioDF['Nome de usuário'].describe())
+            elif option == 2:
+                print(usuarioDF['E-mail'])
+                print(usuarioDF['E-mail'].describe())
+            elif option == 3:
+                nome_desejado = input('Insira o nome desejado: ')
+                if nome_desejado in usuarioDF:
+                    print(usuarioDF.loc['Nome de usuário'] == nome_desejado)
+                    print(usuarioDF.loc['Nome de usuário'].describe() == nome_desejado)
+            elif option == 4:
+                email_desejado = input('Insira o nome desejado: ')
+                if email_desejado in usuarioDF:
+                    print(usuarioDF.loc['E-mail'] == email_desejado)
+                    print(usuarioDF.loc['E-mail'].describe() == email_desejado)
+            elif option == 5:
+                break
